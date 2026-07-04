7811f7350745bb3267d69f8c583b8680548ba522
Jovana Jakovljević <Jovanajako@hotmail.com>
2026-07-04 22:34:14 +0200

    chore: increase Vite chunk warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-      '@assets': resolve(__dirname, 'src/assets'),
-      '@components': resolve(__dirname, 'src/components'),
-      '@data': resolve(__dirname, 'src/data'),
-      '@hooks': resolve(__dirname, 'src/hooks'),
-      '@models': resolve(__dirname, 'src/models'),
-      '@pages': resolve(__dirname, 'src/pages'),
-      '@utils': resolve(__dirname, 'src/utils'),
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+      "@components": resolve(__dirname, "src/components"),
+      "@data": resolve(__dirname, "src/data"),
+      "@hooks": resolve(__dirname, "src/hooks"),
+      "@models": resolve(__dirname, "src/models"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@utils": resolve(__dirname, "src/utils"),
     },
   },
 });
